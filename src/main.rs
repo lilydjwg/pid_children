0bd28d9d6778f7d26ddf3baf4bee5a25a6b0b8c6
依云 <lilydjwg@gmail.com>
2016-06-14 16:46:57 +0800

    support for older kernels
    that don't have /proc/{pid}/task/{tid}/children

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::fs::File;
-use std::io::Read;
+use std::fs::{File,read_dir};
+use std::io::{Read, BufRead, BufReader};
+use std::collections::HashMap;
 
 fn get_children_for_pid(pid: &str) -> Option<Vec<String>> {
     let children = format!("/proc/{0}/task/{0}/children", pid);
@@ -40,9 +41,75 @@ fn get_all_children_for_pid(pid: &str) -> Vec<String> {
     pids
 }
 
+fn get_all_children_for_pid_old_kernel(pid: &str) -> Vec<String> {
+    let mut children_map = HashMap::new();
+
+    for d in read_dir("/proc").unwrap() {
+        let path = d.unwrap().path();
+        let pid = path.file_name().unwrap().to_string_lossy().into_owned();
+        if pid.parse::<usize>().is_ok() {
+            if let Some(ppid) = get_ppid_for(&pid) {
+                let mut children = children_map.entry(ppid).or_insert_with(Vec::new);
+                children.push(pid);
+            }
+        }
+    }
+
+    get_all_children_for_pid_from_map(&children_map, pid)
+}
+
+fn get_all_children_for_pid_from_map(map: &HashMap<String,Vec<String>>, pid: &str) -> Vec<String> {
+    let mut pids = Vec::new();
+    if let Some(children) = map.get(pid) {
+        for pid in children {
+            let grandchildren = get_all_children_for_pid_from_map(map, pid);
+            pids.push(pid.to_string());
+            if !grandchildren.is_empty() {
+                for child in grandchildren {
+                    pids.push(child.to_string());
+                }
+            }
+        }
+    }
+    pids
+}
+
+fn get_ppid_for(pid: &str) -> Option<String> {
+    let status = format!("/proc/{}/status", pid);
+
+    let file = match File::open(&status) {
+        Ok(f) => f,
+        Err(_) => return None,
+    };
+
+    let reader = BufReader::new(file);
+
+    for l in reader.lines() {
+        let line = match l {
+            Ok(s) => s,
+            Err(_) => return None,
+        };
+        if line.starts_with("PPid:") {
+            return Some(line.split_whitespace().nth(1).unwrap().to_string());
+        }
+    }
+
+    None
+}
+
+fn has_children_file() -> bool {
+    let p = std::path::Path::new("/proc/1/task/1/children");
+    p.is_file()
+}
+
 fn main() {
+    let use_children_file = has_children_file();
     for pid in std::env::args().skip(1) {
-        let pids = get_all_children_for_pid(&pid);
+        let pids = if use_children_file {
+            get_all_children_for_pid(&pid)
+        } else {
+            get_all_children_for_pid_old_kernel(&pid)
+        };
         println!("{}", pids.join(" "));
     }
 }
