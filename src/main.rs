a0b9f138fa0c4eb77566afc763c114153cb5c1a9
lilydjwg <lilydjwg@gmail.com>
2017-11-19 18:57:42 +0800

    children file is for tasks, not for processes

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,8 @@
 use std::fs::{File,read_dir};
-use std::io::{Read, BufRead, BufReader};
+use std::io::{BufRead, BufReader};
 use std::collections::HashMap;
 
-fn get_children_for_pid(pid: &str) -> Option<Vec<String>> {
-    let children = format!("/proc/{0}/task/{0}/children", pid);
-
-    let mut file = match File::open(&children) {
-        Ok(f) => f,
-        Err(_) => return None,
-    };
-
-    let mut buf = String::new();
-    match file.read_to_string(&mut buf) {
-        Ok(_) => (),
-        Err(_) => return None,
-    };
-    if buf.is_empty() {
-        return None;
-    }
-
-    Some(buf.split(' ').filter(|x| !x.is_empty()).map(|x| x.to_string()).collect())
-}
-
 fn get_all_children_for_pid(pid: &str) -> Vec<String> {
-    let mut pids = Vec::new();
-
-    let children = match get_children_for_pid(pid) {
-        Some(c) => c,
-        None => return pids,
-    };
-
-    for pid in children {
-        let mut grandchildren = get_all_children_for_pid(&pid);
-        pids.push(pid);
-        if !grandchildren.is_empty() {
-            pids.append(&mut grandchildren);
-        }
-    }
-
-    pids
-}
-
-fn get_all_children_for_pid_old_kernel(pid: &str) -> Vec<String> {
     let mut children_map = HashMap::new();
 
     for d in read_dir("/proc").unwrap() {
@@ -94,19 +55,9 @@ fn get_ppid_for(pid: &str) -> Option<String> {
     None
 }
 
-fn has_children_file() -> bool {
-    let p = std::path::Path::new("/proc/1/task/1/children");
-    p.is_file()
-}
-
 fn main() {
-    let use_children_file = has_children_file();
     for pid in std::env::args().skip(1) {
-        let pids = if use_children_file {
-            get_all_children_for_pid(&pid)
-        } else {
-            get_all_children_for_pid_old_kernel(&pid)
-        };
+        let pids = get_all_children_for_pid(&pid);
         println!("{}", pids.join(" "));
     }
 }
